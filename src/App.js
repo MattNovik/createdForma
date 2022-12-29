418dc3cecb1579dce6ad197583f831c238f2808a
lockdur <matvej.novik@gmail.com>
2022-12-29 17:43:40 +0300

    pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.scss';
-import FormPopup from './components/FormPopup';
 import FormCreated from './components/FormCreated';
 
 function App() {
   return (
     <div className="App">
-      <FormPopup />
-      {/* <FormCreated /> */}
+      <FormCreated
+        color={'red'}
+        realFormName={'Введите свой заголовок'}
+        switches={[
+          { label: 'Имя', checked: true, name: 'name' },
+          { label: 'Телефон', checked: true, name: 'tel' },
+          { label: 'Город', checked: true, name: 'city' },
+          { label: 'Предмет', checked: true, name: 'subject' },
+          { label: 'Размер шрифта', checked: true, name: 'font' },
+          {
+            label: 'Добавить комментарий и файл',
+            checked: true,
+            name: 'commFile',
+          },
+        ]}
+        subjectName={{
+          value: 'Введите название предмета',
+          label: 'Введите название предмета',
+        }}
+        cityName={'Введите название города'}
+        buttonName={'Узнать стоимость'}
+        workType={{ value: undefined, label: 'Не выбрана' }}
+      />
     </div>
   );
 }
